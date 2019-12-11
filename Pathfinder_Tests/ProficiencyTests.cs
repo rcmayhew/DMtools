79877d636af661aa6a61ee1fe0fa28e11f12de8e
rcmayhew <rcmayhewii@gmail.com>
2019-12-10 22:57:18 -0800

    Finished the proficiency tests

--- Pathfinder_Tests/ProficiencyTests.cs
+++ Pathfinder_Tests/ProficiencyTests.cs
@@ -6,9 +6,52 @@ namespace Pathfinder_Tests
     [TestClass]
     public class ProficiencyTests
     {
+        int level = 7;
         [TestMethod]
-        public void TestMethod1()
+        public void UntrainedTest()
         {
+            int expected = 0;
+            Proficiency proficiency = new Proficiency(Levels.untrained, this.level);
+            int actual = proficiency.ProficiencyBonus();
+            Assert.AreEqual(expected, actual, 0.001, "Untrained Bonus not calculating correctly");
+
+        }
+        [TestMethod]
+        public void TrainedTest()
+        {
+            int expected = 9;
+            Proficiency proficiency = new Proficiency(Levels.trained, this.level);
+            int actual = proficiency.ProficiencyBonus();
+            Assert.AreEqual(expected, actual, 0.001, "Trained Bonus not calculating correctly");
+
+        }
+
+        [TestMethod]
+        public void ExpertTest()
+        {
+            int expected = 11;
+            Proficiency proficiency = new Proficiency(Levels.expert, this.level);
+            int actual = proficiency.ProficiencyBonus();
+            Assert.AreEqual(expected, actual, 0.001, "Expert Bonus not calculating correctly");
+
+        }
+        [TestMethod]
+        public void MasterTest()
+        {
+            int expected = 13;
+            Proficiency proficiency = new Proficiency(Levels.master, this.level);
+            int actual = proficiency.ProficiencyBonus();
+            Assert.AreEqual(expected, actual, 0.001, "Master Bonus not calculating correctly");
+
+        }
+        [TestMethod]
+        public void LegendarTest()
+        {
+            int expected = 15;
+            Proficiency proficiency = new Proficiency(Levels.legendary, this.level);
+            int actual = proficiency.ProficiencyBonus();
+            Assert.AreEqual(expected, actual, 0.001, "Legendary Bonus not calculating correctly");
+
         }
     }
 }
