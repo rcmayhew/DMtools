1537609f81fc221d648f4c458dcd8346268454be
rcmayhew <rcmayhewii@gmail.com>
2019-12-17 22:49:08 -0800

    Added Attributes

---
+++ Pathfinder/Attribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pathfinder
+{
+    class Attribute
+    {
+        int score;
+        int mod;
+
+        private void CalculateMod()
+        {
+            //this should be ran everytime score is updated
+            this.mod = (this.score - 10) / 2; //because of int trucation, this should be the same as floor 
+        }
+        Attribute()
+        {
+            this.score = 10;
+            this.CalculateMod();
+        }
+
+        Attribute(int score)
+        {
+            this.score = score;
+            this.CalculateMod();
+        }
+
+        public int GetScore()
+        {
+            return this.score;
+        }
+
+        public int GetMod()
+        {
+            return this.mod;
+        }
+    }
+}
